f227c7bd02b3dd9d393a64a2cfbe55228502f38e
Ulugbek J. <muskisgenius@gmail.com>
2025-03-05 21:24:49 +0500

    Admin Page

--- config/settings.py
+++ config/settings.py
@@ -139,12 +139,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/food/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 
 
 MEDIA_ROOT = BASE_DIR.joinpath("media")
-MEDIA_URL = '/media/food/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- food/models.py
+++ food/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    category = models.ForeignKey(Category, null=False, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=False, on_delete=models.CASCADE)
     cost = models.IntegerField(null=False, blank=False)
     price = models.IntegerField(null=False, blank=False)
     image = models.ImageField(upload_to='products')
@@ -38,7 +38,7 @@ class Order(models.Model):
     payment_type = models.IntegerField(null=False, blank=False)
     status = models.IntegerField(null=False, blank=False, default=1)
     address = models.CharField(max_length=250, null=False, blank=False)
-    customer = models.ForeignKey(Customer, null=False, on_delete=models.SET_NULL)
+    customer = models.ForeignKey(Customer, null=False, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -49,8 +49,6 @@ class OrderProduct(models.Model):
     first_name = models.CharField(max_length=100, null=False, blank=False)
     last_name = models.CharField(max_length=100, null=False, blank=False)
     age = models.IntegerField(null=False, blank=False)
-    subject = models.ForeignKey(Subject, null=True, on_delete=models.CASCADE)
-    kafedra = models.ForeignKey(Kafedra, null=True, on_delete=models.SET_NULL)
 
 
 
