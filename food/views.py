984ed56d965124ce252958ac86de4d5ff46bfd3e
Ulugbek J. <muskisgenius@gmail.com>
2025-03-22 09:38:51 +0500

    Admin Page

--- food/views.py
+++ food/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render, redirect
 from config.settings import MEDIA_ROOT
 from .models import *
+from forms import *
+
+import json
 # Create your views here.
+
+
 def index(request):
-    titles = Category.title
-    title = Product.title
-    description = Product.description
-    price = Product.price
-    image = Product.image
+    titles = Category.objects.all()
+    products = Product.objects.all()
+    orders = []
+    order_list = request.COOKIES.get("orders")
+    total_price = request.COOKIES.get("total_price", 0)
+
+    if order_list:
+        for key, value in json.loads(order_list).item():
+            orders.append(
+                {
+                    "product": Product.objects.get(pk=int(key)),
+                    "count": value
+                }
+            )
+
 
     ctx = {
         'titles': titles,
-        'title': title,
-        'description': description,
-        'price': price,
-        'image': image,
+        'products': products,
+        'orders': orders,
+        'total_price': total_price,
         'MEDIA_ROOT': MEDIA_ROOT
     }
 
